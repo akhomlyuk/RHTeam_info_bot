5ce970e152d307259fd12b39646883b4da6b9061
akhomlyuk <relicbs@gmail.com>
2023-07-26 16:29:36 +0300

    v 0.2

--- web/db_functions.py
+++ web/db_functions.py
@@ -6,6 +6,7 @@ def show_users():
     users_list = []
     for user in users:
         # users_list.append([user.id, user.name, user.group, user.active])
-        users_list.append(f"ID: <b>{user.id}</b> Имя: <b>{user.name}</b> Группа: <b>{user.group}</b> Активен: <b>{user.active}</b>")
-
+        # users_list.append(f"ID: <b>{user.id}</b> Имя: <b>{user.name}</b> Группа: <b>{user.group}</b> Активен: <b>{user.active}</b>")
+        # users_list.append(f"ID: {user.id} Имя: {user.name} Группа: {user.group} Активен: {user.active}")
+        users_list.append({'id': user.id, 'name': user.name, 'group': user.group, 'active': user.active})
     return users_list

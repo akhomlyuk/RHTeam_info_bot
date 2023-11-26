dc3a20d0f3f18c6511f420cf91bfa2e3ebc5d0e9
akhomlyuk <relicbs@gmail.com>
2023-11-26 16:20:31 +0300

    v 0.3 reply to answer

--- web/helpers.py
+++ web/helpers.py
@@ -6,10 +6,11 @@ def images_to_index():
     try:
         img_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
         image_list = []
-        file_list = os.listdir(img_dir)
+        file_list = sorted(os.scandir(img_dir), key=lambda f: os.path.getmtime(f.path))
+
         for file in file_list:
-            if file.endswith(".png") or file.endswith(".jpg"):
-                image_list.append(file)
+            if file.name.endswith(".png") or file.name.endswith(".jpg"):
+                image_list.append(file.name)
         return image_list
     except Exception as e:
         ic(e)

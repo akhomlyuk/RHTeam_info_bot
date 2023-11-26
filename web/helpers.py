87e7a7c932eb6aed5b15bbf79bd546a3d5469991
akhomlyuk <relicbs@gmail.com>
2023-11-26 16:25:03 +0300

    v 0.3 reply to answer

--- web/helpers.py
+++ web/helpers.py
@@ -6,7 +6,7 @@ def images_to_index():
     try:
         img_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
         image_list = []
-        file_list = sorted(os.scandir(img_dir), key=lambda f: os.path.getmtime(f.path))
+        file_list = sorted(os.scandir(img_dir), key=lambda f: os.path.getmtime(f.path), reverse=True)
 
         for file in file_list:
             if file.name.endswith(".png") or file.name.endswith(".jpg"):
